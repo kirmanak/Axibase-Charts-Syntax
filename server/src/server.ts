af4c9007c5e6e8abe407a4d326d0075d1e5c8934
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-06-21 17:35:19 +0300

    language server connects successfully

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
-import { 
-    createConnection, ProposedFeatures, TextDocuments, 
-    InitializeParams, DidChangeConfigurationNotification, 
-    TextDocumentChangeEvent, TextDocument,
-    Diagnostic, DiagnosticSeverity
-} from "vscode-languageserver";
+'use strict';
+
+import {
+	createConnection, TextDocuments, TextDocument, Diagnostic, DiagnosticSeverity,
+	ProposedFeatures, InitializeParams, DidChangeConfigurationNotification
+} from 'vscode-languageserver';
 
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -17,25 +17,21 @@ let hasConfigurationCapability: boolean = false;
 let hasWorkspaceFolderCapability: boolean = false;
 let hasDiagnosticRelatedInformationCapability: boolean = false;
 
+
 connection.onInitialize((params: InitializeParams) => {
-    let capabilities = params.capabilities;
+	let capabilities = params.capabilities;
 
-    // Does the client support the `workspace/configuration` request?
-    // If not, we will fall back using global settings
-    hasConfigurationCapability = capabilities.workspace && !!capabilities.workspace.configuration;
-    hasWorkspaceFolderCapability = capabilities.workspace && !!capabilities.workspace.workspaceFolders;
-    // hasDiagnosticRelatedInformationCapability = capabilities.textDocument && capabilities.textDocument.publishDiagnostics
-    //     && capabilities.textDocument.publishDiagnostics.relatedInformation;
+	// Does the client support the `workspace/configuration` request?
+	// If not, we will fall back using global settings
+	hasConfigurationCapability = capabilities.workspace && !!capabilities.workspace.configuration;
+	hasWorkspaceFolderCapability = capabilities.workspace && !!capabilities.workspace.workspaceFolders;
+	hasDiagnosticRelatedInformationCapability = capabilities.textDocument && capabilities.textDocument.publishDiagnostics && capabilities.textDocument.publishDiagnostics.relatedInformation;
 
-    return {
-        capabilities: {
-            textDocumentSync: documents.syncKind,
-            // Tell the client that the server supports code completion
-            completionProvider: {
-                resolveProvider: true
-            }
-        }
-    }
+	return {
+		capabilities: {
+			textDocumentSync: documents.syncKind
+		}
+	}
 });
 
 connection.onInitialized(() => {
@@ -50,19 +46,13 @@ connection.onInitialized(() => {
 	}
 });
 
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent((change: TextDocumentChangeEvent) => {
-	connection.console.log(change.document.getText());
-});
-
-// The Server settings
+// The example settings
 interface ServerSettings {
 	maxNumberOfProblems: number;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this Server
+// Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
 const defaultSettings: ServerSettings = { maxNumberOfProblems: 1000 };
 let globalSettings: ServerSettings = defaultSettings;
@@ -75,11 +65,11 @@ connection.onDidChangeConfiguration(change => {
 		// Reset all cached document settings
 		documentSettings.clear();
 	} else {
-		globalSettings = <ServerSettings>(change.settings.languageServerServer || defaultSettings);
+		globalSettings = <ServerSettings>(change.settings.languageServer || defaultSettings);
 	}
 
 	// Revalidate all open text documents
-	// documents.all().forEach(validateTextDocument);
+	documents.all().forEach(validateTextDocument);
 });
 
 function getDocumentSettings(resource: string): Thenable<ServerSettings> {
@@ -88,11 +78,16 @@ function getDocumentSettings(resource: string): Thenable<ServerSettings> {
 	}
 	let result = documentSettings.get(resource);
 	if (!result) {
-		result = connection.workspace.getConfiguration({ scopeUri: resource, section: 'languageServerServer' });
+		result = connection.workspace.getConfiguration({ scopeUri: resource, section: 'languageServer' });
 		documentSettings.set(resource, result);
 	}
 	return result;
 }
+
+// Only keep settings for open documents
+documents.onDidClose(e => {
+	documentSettings.delete(e.document.uri);
+});
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
@@ -147,7 +142,34 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
-// Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
+connection.onDidChangeWatchedFiles((_change) => {
+	// Monitored files have change in VSCode
+	connection.console.log('We received an file change event');
 });
+
+/*
+connection.onDidOpenTextDocument((params) => {
+	// A text document got opened in VSCode.
+	// params.uri uniquely identifies the document. For documents store on disk this is a file URI.
+	// params.text the initial full content of the document.
+	connection.console.log(`${params.textDocument.uri} opened.`);
+});
+connection.onDidChangeTextDocument((params) => {
+	// The content of a text document did change in VSCode.
+	// params.uri uniquely identifies the document.
+	// params.contentChanges describe the content changes to the document.
+	connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
+});
+connection.onDidCloseTextDocument((params) => {
+	// A text document got closed in VSCode.
+	// params.uri uniquely identifies the document.
+	connection.console.log(`${params.textDocument.uri} closed.`);
+});
+*/
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
