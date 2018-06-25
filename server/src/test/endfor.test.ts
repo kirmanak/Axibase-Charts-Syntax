3328b48d774d0c6041372fef1fee5adc5e6c39a2
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-06-25 18:40:58 +0300

    prettify endfor tests

--- server/src/test/endfor.test.ts
+++ server/src/test/endfor.test.ts
@@ -4,33 +4,33 @@ import * as Functions from '../validateFunctions';
 
 suite("Unmatched endfor tests", () => {
 
-    test("Correct one loop", () => {
-        const text = 
-        "for server in servers\n"+
-        "   #do something\n"+
-        "endfor";
+    test("One correct loop", () => {
+        const text =
+            "for server in servers\n" +
+            "   #do something\n" +
+            "endfor";
         const document: TextDocument = TextDocument.create(
             "testDoc", // uri
             "atsd-visual", // language id
-             0, // version
+            0, // version
             text // content
         );
         const result = Functions.unmatchedEndFor(document, true);
         assert.equal(result.length, 0);
     });
 
-    test("Correct two loop", () => {
-        const text = 
-        "for server in servers\n"+
-        "   #do something\n"+
-        "endfor\n"+
-        "for server in servers\n"+
-        "   #do something\n"+
-        "endfor";
+    test("Two correct loops", () => {
+        const text =
+            "for server in servers\n" +
+            "   #do something\n" +
+            "endfor\n" +
+            "for server in servers\n" +
+            "   #do something\n" +
+            "endfor";
         const document: TextDocument = TextDocument.create(
             "testDoc", // uri
             "atsd-visual", // language id
-             0, // version
+            0, // version
             text // content
         );
         const result = Functions.unmatchedEndFor(document, true);
