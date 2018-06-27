dd86d745ae5f3382d620919255c09a293a1bb07f
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-06-27 17:43:33 +0300

    replace comments with spaces instead of deleting

--- server/src/sharedFunctions.ts
+++ server/src/sharedFunctions.ts
@@ -1,10 +1,16 @@
 export function deleteComments(text: string): string {
 	const multiLine = /\/\*[\s\S]*\*\//gm;
 	const oneLine = /#.*/g;
-	let matching: RegExpExecArray;
+	const notNewLine = /./g;
+	let i, j: RegExpExecArray;
 
-	while ((matching = multiLine.exec(text)) || (matching = oneLine.exec(text))) {
-		text = text.substring(0, matching.index) + text.substring(matching.index + matching[0].length, text.length);
+	while ((i = multiLine.exec(text)) || (i = oneLine.exec(text))) {
+		let comment = i[0];
+		while (j = notNewLine.exec(comment)) {
+			comment = comment.substring(0, j.index) + " " + comment.substring(j.index + 1);
+		}
+		text = text.substring(0, i.index) + comment +
+			text.substring(i.index + comment.length, text.length);
 	}
 
 	return text;
