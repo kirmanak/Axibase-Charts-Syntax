0dadfa00f2f9cf5cba8f9b29597dbc639fac9d91
Kirill Kamakin <k.kamakin@protonmail.ch>
2018-08-13 14:26:40 +0300

    Snippets (#186)
    * Add csv snippet
    
    * Change if and for snippets
    
    * Provide more complex snippets
    
    * Fix completion item from closed for

--- server/src/completionProvider.ts
+++ server/src/completionProvider.ts
@@ -15,6 +15,7 @@ export class CompletionProvider {
 
     public getCompletionItems(): CompletionItem[] {
         return this.completeSettings()
+            .concat(this.completeIf())
             .concat([this.completeFor()]);
     }
 
@@ -40,11 +41,80 @@ export class CompletionProvider {
 
         return {
             detail: "For Loop",
-            insertText: `\nfor \${1:${item}} in \${2:${collection}}\n  \${0}\nendfor`,
+            insertText: `
+for \${1:${item}} in \${2:${collection}}
+  \${3:entity = @{\${1:${item}}}}
+  \${0}
+endfor`,
             insertTextFormat: InsertTextFormat.Snippet,
             kind: CompletionItemKind.Keyword,
             label: "for",
         };
+    }
+
+    private completeIf(): CompletionItem[] {
+        const regexp: RegExp = /^[ \t]*for[ \t]+(\w+)[ \t]+in/img;
+        const endFor: RegExp = /^[ \t]*endfor/img;
+        let match: RegExpExecArray = regexp.exec(this.text);
+        let lastMatch: RegExpExecArray;
+
+        while (match) {
+            const end: RegExpExecArray = endFor.exec(this.text);
+            if (!end || end.index < match.index) {
+                lastMatch = match;
+            }
+            match = regexp.exec(this.text);
+        }
+
+        let item: string = "item";
+
+        if (lastMatch) {
+            item = lastMatch[1];
+        }
+
+        return [
+            {
+                detail: "if item equals text",
+                insertText: `
+if @{\${1:${item}}} \${2:==} \${3:"item1"}
+  \${4:entity} = \${5:"item2"}
+else
+  \${4:entity} = \${6:"item3"}
+endif
+\${0}`,
+                label: "if text",
+            },
+            {
+                detail: "if item equals number",
+                insertText: `
+if @{\${1:${item}}} \${2:==} \${3:5}
+  \${4:entity} = \${5:"item1"}
+else
+  \${4:entity} = \${6:"item2"}
+endif
+\${0}`,
+                label: "if number",
+            },
+            {
+                detail: "if item equals number else if",
+                insertText:
+                    `
+if @{\${1:${item}}} \${2:==} \${3:5}
+  \${4:entity} = \${5:"item1"}
+elseif @{\${1:${item}}} \${6:==} \${7:6}
+  \${4:entity} = \${8:"item2"}
+else
+  \${4:entity} = \${9:"item3"}
+endif
+\${0}`,
+                label: "if elseif",
+            },
+        ].map((completion: CompletionItem): CompletionItem => {
+            completion.insertTextFormat = InsertTextFormat.Snippet;
+            completion.kind = CompletionItemKind.Keyword;
+
+            return completion;
+        });
     }
 
     private readonly completeSettings: () => CompletionItem[] = (): CompletionItem[] =>
